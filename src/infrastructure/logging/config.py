ab1f8484d2ee41d2d5fd3e38e10108597d90b982
Ivan Flores <ifloresmanc96@gmail.com>
2025-08-05 20:06:20 -0300

    config logger consider env var

--- src/infrastructure/logging/config.py
+++ src/infrastructure/logging/config.py
@@ -1,14 +1,18 @@
 import logging
 import structlog
 
+import os
+
+is_dev = os.getenv('PY_ENV', 'dev') == 'dev'
+
 structlog.configure(
-    processors=[
+     processors=[
         structlog.contextvars.merge_contextvars,
         structlog.processors.add_log_level,
         structlog.processors.StackInfoRenderer(),
         structlog.dev.set_exc_info,
         structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
-        structlog.dev.ConsoleRenderer()
+        structlog.dev.ConsoleRenderer(exception_formatter=structlog.dev.better_traceback) if is_dev else structlog.processors.JSONRenderer(),  
     ],
     wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
     context_class=dict,
