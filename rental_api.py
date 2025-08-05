fbe55b46ada5ba4df46ca98accae903073656406
Ivan Flores <ifloresmanc96@gmail.com>
2025-08-05 20:07:31 -0300

    include bg job on start api

--- rental_api.py
+++ rental_api.py
@@ -3,12 +3,21 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.presentation.controllers import departments, health
 
+from contextlib import asynccontextmanager
+from src.infrastructure.jobs.background_service import scheduler
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.start()
+    yield
+    scheduler.shutdown()
 
 app = FastAPI(
     title="Buenos Aires Rental Prices API",
     description="API para consultar precios de alquileres en Buenos Aires",
-    version="1.0.0"
+    version="1.0.0",
+    lifespan=lifespan
 )
 
 app.add_middleware(
